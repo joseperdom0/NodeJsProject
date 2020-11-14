80cdfb502b5bbaea2f2c33af0f468e4b6a8acef7
Joseperdom0 <aguilar.roy@gmail.com>
2020-11-14 14:55:12 -0600

    Added DB URI

--- server/config/db.js
+++ server/config/db.js
@@ -11,4 +11,5 @@ module.exports =
 {
     //"URI" : "mongodb://localhost/book_store"
    "URI" : "mongodb+srv://jose:isPLapzH2uu7NJiI@mongodbserver.b6sbn.mongodb.net/book_store?retryWrites=true&w=majority"
+   //"URI" : "mongodb+srv://jose:isPLapzH2uu7NJiI@mongodbserver.b6sbn.mongodb.net/survey_db?retryWrites=true&w=majority"
 }
