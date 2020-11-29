965e0751bd8a2f54c61961c449ae524a84a4c820
Joseperdom0 <aguilar.roy@gmail.com>
2020-11-29 15:08:47 -0600

    Completed functionality of the Answer survey

--- server/config/app.js
+++ server/config/app.js
@@ -58,6 +58,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../../public')));
 app.use(express.static(path.join(__dirname,'../../node_modules')));
+
 app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
 
 
